a876fb11dd2128810b1de88ae9f438a1eaffdbc0
Steve Herrell <steve.herrell@gmail.com>
2023-11-23 07:05:06 -0500

    Tidy the docs.

--- custom_components/virtual/cfg.py
+++ custom_components/virtual/cfg.py
@@ -186,7 +186,11 @@ def _parse_old_config(devices, configs, platform):
       will only be one entity per device
     """
     for config in configs:
+        if not isinstance(config, dict):
+            _LOGGER.debug(f"not dictionary={config}")
+            continue
         if config[CONF_PLATFORM] != COMPONENT_DOMAIN:
+            _LOGGER.debug(f"not platform={config}")
             continue
 
         # Copy and fix up config.
