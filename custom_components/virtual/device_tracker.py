d3abf404ea5f9f2bf405e2287c12317b58823041
Steve Herrell <47094394+twrecked@users.noreply.github.com>
2024-08-15 12:02:09 -0400

    Device tracker add gps (#120)
    * Add GPS to device tracker.
    
    * Add GPS to device tracker.
    
    * Add GPS to device tracker.
    
    * Add GPS to device tracker.

--- custom_components/virtual/device_tracker.py
+++ custom_components/virtual/device_tracker.py
@@ -13,8 +13,13 @@ from homeassistant.components.device_tracker import (
     SourceType,
     TrackerEntity,
 )
+from homeassistant.components.zone import ATTR_RADIUS
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_ENTITY_ID
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    ATTR_LATITUDE,
+    ATTR_LONGITUDE
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
 
@@ -28,6 +33,7 @@ _LOGGER = logging.getLogger(__name__)
 DEPENDENCIES = [COMPONENT_DOMAIN]
 
 CONF_LOCATION = 'location'
+CONF_GPS = 'gps'
 DEFAULT_DEVICE_TRACKER_VALUE = 'home'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(virtual_schema(DEFAULT_DEVICE_TRACKER_VALUE, {
@@ -38,7 +44,12 @@ DEVICE_TRACKER_SCHEMA = vol.Schema(virtual_schema(DEFAULT_DEVICE_TRACKER_VALUE, 
 SERVICE_MOVE = "move"
 SERVICE_SCHEMA = vol.Schema({
     vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
-    vol.Required(CONF_LOCATION): cv.string,
+    vol.Optional(CONF_LOCATION): cv.string,
+    vol.Optional(CONF_GPS): {
+        vol.Required(ATTR_LATITUDE): cv.latitude,
+        vol.Required(ATTR_LONGITUDE): cv.longitude,
+        vol.Optional(ATTR_RADIUS): cv.string,
+    },
 })
 
 
@@ -84,6 +95,7 @@ class VirtualDeviceTracker(TrackerEntity, VirtualEntity):
         super().__init__(config, PLATFORM_DOMAIN)
 
         self._location = None
+        self._coords = {}
 
         _LOGGER.debug(f"{self._attr_name}, available={self._attr_available}")
         _LOGGER.debug(f"{self._attr_name}, entity={self.entity_id}")
@@ -96,12 +108,23 @@ class VirtualDeviceTracker(TrackerEntity, VirtualEntity):
     def _restore_state(self, state, config):
         _LOGGER.debug(f"device_tracker-restore=state={state.state}")
         _LOGGER.debug(f"device_tracker-restore=attrs={state.attributes}")
+
         if ATTR_AVAILABLE not in state.attributes:
             _LOGGER.debug("looks wrong, from upgrade? creating instead...")
             self._create_state(config)
+            return
+
+        super()._restore_state(state, config)
+        if ATTR_LONGITUDE in state.attributes and ATTR_LATITUDE in state.attributes:
+            self._location = None
+            self._coords = {
+                ATTR_LONGITUDE: state.attributes[ATTR_LONGITUDE],
+                ATTR_LATITUDE: state.attributes[ATTR_LATITUDE],
+                ATTR_RADIUS: 0
+            }
         else:
-            super()._restore_state(state, config)
             self._location = state.state
+            self._coords = {}
 
     @property
     def location_name(self) -> str | None:
@@ -115,20 +138,41 @@ class VirtualDeviceTracker(TrackerEntity, VirtualEntity):
     @property
     def latitude(self) -> float | None:
         """Return latitude value of the device."""
-        return None
+        return self._coords.get(ATTR_LATITUDE, None)
 
     @property
     def longitude(self) -> float | None:
         """Return longitude value of the device."""
-        return None
+        return self._coords.get(ATTR_LONGITUDE, None)
 
-    def move(self, new_location):
+    def move_to_location(self, new_location):
         _LOGGER.debug(f"{self._attr_name} moving to {new_location}")
         self._location = new_location
+        self._coords = {}
+        self.async_schedule_update_ha_state()
+
+    def move_to_coords(self, new_coords):
+        _LOGGER.debug(f"{self._attr_name} moving via GPS to {new_coords}")
+        self._location = None
+        self._coords = new_coords
         self.async_schedule_update_ha_state()
 
 
 async def async_virtual_move_service(hass, call):
     for entity_id in call.data['entity_id']:
-        _LOGGER.debug(f"moving {entity_id}")
-        get_entity_from_domain(hass, PLATFORM_DOMAIN, entity_id).move(call.data[CONF_LOCATION])
+        _LOGGER.debug(f"moving {entity_id} --> {call.data}")
+
+        entity = get_entity_from_domain(hass, PLATFORM_DOMAIN, entity_id)
+        if entity is None:
+            _LOGGER.debug(f"can't find {entity_id}")
+            return
+
+        location = call.data.get(CONF_LOCATION, None)
+        coords = call.data.get(CONF_GPS, None)
+        if location is not None:
+            entity.move_to_location(location)
+        elif coords is not None:
+            entity.move_to_coords(coords)
+        else:
+            _LOGGER.debug(f"not moving {entity_id}")
+
